edd8f4f7d6324ac2a434816b6476df5f69e5d453
MustafovaSemra <mustafovasemra@yahoo.com>
2021-06-29 15:58:33 -0400

    done-missing css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import contacts from "./contacts.json";
 
 function App() {
+  const [list, setList] = useState(contacts.slice(0, 5));
+  const [rest, setRest] = useState(contacts);
+
+  const addRandomContact = (i) => {
+    let restOfContacts = [...rest];
+    let randomIndex = Math.floor(Math.random() * restOfContacts.length);
+    let random = restOfContacts[randomIndex];
+    let copyOfList = [...list];
+    copyOfList.push(random);
+    setList(copyOfList);
+    let copyOfRest = [...rest];
+    copyOfRest.splice(randomIndex, 1);
+  };
+
+  const sortByPopularity = () => {
+    let popularList = [...list].sort((a, b) => b.popularity - a.popularity);
+    setList(popularList);
+  };
+
+  const sortByName = () => {
+    let sortedByName = [...list].sort((a, b) => a.name.localeCompare(b.name));
+    setList(sortedByName);
+  };
+
+  const deleteName = (i) => {
+    let copyOfList = [...list];
+    copyOfList.splice(i, 1);
+    setList(copyOfList);
+  };
+
+  const FirstFive = () => {
+    let listOfFirstFive = list.map((eachCeleb) => {
+      return (
+        <table>
+          <td>
+            <img src={eachCeleb.pictureUrl} style={{ height: 100 }} />
+          </td>
+          <td>{eachCeleb.name}</td>
+          <td>{eachCeleb.popularity.toFixed(2)}</td>
+          <td>
+            <button onClick={deleteName}>Delete</button>
+          </td>
+        </table>
+      );
+    });
+    return listOfFirstFive;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Iron Contacts</h1>
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={sortByName}>Sort by Name</button>
+      <button onClick={sortByPopularity}>Sort by Popularity</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Populatirty</th>
+          </tr>
+        </thead>
+        <tbody>
+          <FirstFive />
+        </tbody>
+      </table>
     </div>
   );
 }
